be2a9b18240400e4c9b6fd8b932aba018f1724d9
That2D <forever.derpy@gmail.com>
2022-04-25 10:51:13 -0500

    start of website

---
+++ storefront.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Group 22's Site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+    <h1>Group 22's Site</h1>
+    <h2>Storefront</h2>
+    <nav>
+        <ul>
+            <li><a href="storefront.php">Storefront</a></li>
+            <li><a href="cart.php">Cart</a></li>
+            <li><a href="checkout.php">Checkout</a></li>
+            <li><a href="inventory.php">Inventory</a></li>
+        </ul>
+    </nav>
+
+    <hr>
+    <h2>Products</h2>
+</body>
+</html>
