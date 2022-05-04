7663082ff5dfd03aa051edb5e0ee44c0db534d97
Jordan Raia <z1887657@hopper.cs.niu.edu>
2022-05-03 23:33:28 -0500

    woops

--- password.php
+++ password.php
@@ -1,5 +1,5 @@
 <?php
-    $dbname = "mysql:host=courses;dbname=z1887657";
-    $user = "z1887657";
-    $pass = "1999Mar23";
+    $dbname = "mysql:host=courses;dbname=Zxxxxxxx";
+    $user = "Zxxxxxxx";
+    $pass = "YYYYMmmDD";
 ?>
