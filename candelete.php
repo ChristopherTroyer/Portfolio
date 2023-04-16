5b09fdd01a4cd0de8d3ba53670129214944a8fd7
holzel <holzel@pop-os.localdomain>
2023-04-16 14:47:09 -0500

    added db creation script, for testing

---
+++ candelete.php
@@ -0,0 +1,38 @@
+<?php
+
+// create a table inside the database, it keeps creating it everytime its run
+// so it only needs to be run once
+
+$dbhost = "localhost"; //where to connect
+$dbuser = "root"; //usename
+$dbpass = "password"; // password
+$dbname = "testing"; //database name
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if(! $conn) { // throw an error if the connection to the db fails
+    echo 'conn failure!';
+} else {
+    echo 'connected!';
+}
+
+
+/*
+    create table in db named "users"
+    usersId is the primary key
+*/
+$sql = "CREATE TABLE users (
+usersId int(11) AUTO_INCREMENT,
+usersName varchar(128) NOT NULL,
+usersEmail varchar(128) NOT NULL,
+usersUid varchar(128) NOT NULL,
+usersPass varchar(128) NOT NULL,
+primary key(usersId)
+)";
+
+if(mysqli_query($conn, $sql)) { // throw error if the query failed
+    echo 'Db created!';
+} else {
+    echo 'there was some error: '.mysqli_error($conn);
+}
+
+mysqli_close($conn); // close connection
