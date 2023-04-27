99cddc5b960187a51444c55703fe97940423fcf0
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-04-27 17:41:24 -0500

    Update admin_home_page.php
    Throwing shit in before css, idk if it will work

--- admin_home_page.php
+++ admin_home_page.php
@@ -9,6 +9,7 @@
 
 <main>This is a test for admin page</main>
 
+<html>
 <?php
     session_start();
 
@@ -31,6 +32,15 @@
         echo "Connection failed: " . &e->getMessage();
     }
 ?>
+    <body>
+        <form method="POST">
+            <br />
+            <input type="submit" name="associate" value="Associates">
+            <input type="submit" name="quote" value="Quotes">
+            <br />
+        </form>
+    </body>
+</html>
 
 <?php
  include_once 'footer.php'
