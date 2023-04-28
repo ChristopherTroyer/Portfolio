9baae912d4b4c5854aaaf2d7f05b459a28d8e325
h-morales1 <hmorales5@tutanota.com>
2023-04-27 19:53:06 -0500

    drop down populated with customer names from legacy db

--- associate.php
+++ associate.php
@@ -3,6 +3,26 @@
 ?>
 
 <main>This is a test for associate page</main>
+<br></br>
+
+<body> 
+    <p>Select a customer to create a new quote:</p>
+    <form> 
+        <select> 
+            <option selected="selected">Choose Customer</option>
+
+<?php
+    require_once 'includes/legacydbh.inc.php'; // legacy database handler
+    require_once 'includes/functions.inc.php'; // utility functions for error checking credentials / login utils
+
+    $customersArray = fillArray($conn, "SELECT * FROM customers;"); //retrieve all customers in legacy db, place into array
+    fillDropDown($customersArray, "name"); // populate dropdown with the name column from the array
+?>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
 <?php
  include_once 'footer.php'
 ?>
