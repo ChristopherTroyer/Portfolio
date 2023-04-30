38761ca22b22d6aacfe934dd4f75d27f3cb11f71
Chris <forever.derpy@gmail.com>
2023-04-30 14:56:38 -0500

    Candelete now uses sql table generation script.

--- candelete.php
+++ candelete.php
@@ -4,8 +4,8 @@
 // so it only needs to be run once
 
 $dbhost = "localhost"; //where to connect
-$dbuser = "root"; //usename
-$dbpass = "password"; // password
+$dbuser = "debian-sys-maint"; //usename
+$dbpass = "vUlvuFil887Af63z"; // password
 $dbname = "testing"; //database name
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
@@ -15,23 +15,12 @@ if(! $conn) { // throw an error if the connection to the db fails
     echo 'connected!';
 }
 
-
 /*
-    create table in db named "users"
-    usersId is the primary key
-    usersPerms: 0 = associate, 1 = hq, 2 = admin
+    runs the Quote_Tables.sql file
 */
-$sql = "CREATE TABLE users (
-    usersId int(11) AUTO_INCREMENT,
-    usersPerms int(11) NOT NULL DEFAULT 0,
-    usersName varchar(128) NOT NULL,
-    usersEmail varchar(128) NOT NULL,
-    usersUid varchar(128) NOT NULL,
-    usersPass varchar(128) NOT NULL,
-    primary key(usersId)
-    )";
+$sql = file_get_contents("Quote_Tables.sql");
 
-if(mysqli_query($conn, $sql)) { // throw error if the query failed
+if(mysqli_multi_query($conn, $sql)) { // throw error if the query failed
     echo 'Db created!';
 } else {
     echo 'there was some error: '.mysqli_error($conn);
