bdb37566cea1f3a2f25e7ee27518419fd10c923c
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-05-01 00:33:45 -0500

    Update admin_view_assoc.php
    This should work, I do need to test with our server and make sure it can take associates properly.

--- admin_view_assoc.php
+++ admin_view_assoc.php
@@ -1,47 +1,62 @@
 <!--
     This allows the admin to view, edit, add, and delete
-    associates in the company. 
+    associates in the company.
 -->
 <?php
     include_once 'header.php';
 ?>
 
-<main>This is a test for showing associates</main>
+<br />
+<h2>Current Associates</h2>
+<br />
 
-<html>
 <?php
     require_once 'includes/legacydbh.inc.php';
     require_once 'includes/functions.inc.php';
-    require_once 'admin/adminfunctions.inc.php';
+    require_once 'adminfunctions.inc.php'; //I didn't know if I threw functions in the includes if it would be ok
+                                           //I dont want to wake people from slumber Mimimimi
 
-    function show_all()
+    function show_all_assoc()
     {
+        //build table for associate preview information
         echo
         ("
-            <tablee border=solid>
+            <table border=solid>
             <tr>
-            <th> Associate ID </th>
+            <th> ID </th>
+            <th> Username </th>
             <th> First Name </th>
             <th> Last Name </th>
-            <th> Username </th>
-            <th> Password </th>
-            <th> Email </th>
-            <th> Address </th>
             <th> Commission </th>
-            <th> Permission </th>
             </tr>
         ");
-        $ID = show_all();
-        
+        //finds all the associate IDS in the dbh
+        $ID = associateIDS();
+        foreach($ID as $arrID)
+        {
+            //gets all information for each assoiate in dbh
+            $assoc = associateinfo($arrID);
+            
+            //small amount like a preview
+            echo ("<tr>");
+                echo ("<td>" . $arrID . "</td>");
+                echo ("<td>" . $assoc['username'] . "</td>");
+                echo ("<td>" . $assoc['First_name'] . "</td>");
+                echo ("<td>" . $assoc['last_name'] . "</td>");
+                echo ("<td>" . $assoc['commission'] . "</td>");
+                echo '<td><a href="view_all_info.php?if='.$arrID.'">View</a></td>'; //view all information
+                echo '<td><a href="update_assoc.php?id='.$arrID.'">Update</a></td>'; //update and edit associate info
+                echo '<td><a href="delete_assoc.php?id='.$arrID.'">Delete</a></td>'; //delete associate from dbh
+            echo ("</tr>");
+        }
+    }
+
+    try
+    {
+        show_all_assoc();
     }
     catch(PDOexception $e)
     {
-        echo "Connection failed: " . &e->getMessage();
+        echo "Connection failed: " . $e->getMessage();
     }
-?>
-
-</html>
-
-<?php
-    include_once 'footer.php';
 ?>
