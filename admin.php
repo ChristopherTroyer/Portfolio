393c17584e47f8168a5696a0ca91e57e4b1eba7b
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-05-01 22:13:06 -0500

    Update admin.php
    added the ability to view associates to home page, the button was broken and instead of running in circles why not make it apart of the home page.

--- admin.php
+++ admin.php
@@ -7,20 +7,18 @@
  include_once 'header.php';
 ?>
 
-<main>This is a test for admin page</main>
-
 <html>
 <?php
-    require_once 'includes/legacydbh.inc.php';
-    require_once 'includes/functions.inc.php';
+    require_once 'legacydbh.inc.php';
+    require_once 'functions.inc.php';
 
     try
     {
-        if(isset($_POST["assoc"]))
-        {
+//        if(isset($_POST["assoc"]))
+//        {
             //this will have everything associate related
-            header("location:admin_view_assoc.php");
-        }
+//            header("location:admin_view_assoc.php");
+//        }
         if(isset($_POST["quote"]))
         {
             //this will have everything quote related
@@ -36,14 +34,70 @@
     <body>
         <form method="POST">
             <br />
-            <input type="submit" name="associate" value="Associates">
+            <!-- This button was broken, I brought page to the button -->
+<!--            <input type="submit" name="associate" value="Associate"> -->
             <input type="submit" name="quote" value="Quotes">
             <br />
         </form>
     </body>
 </html>
 
+
+<!--
+    This allows the admin to view, edit, add, and delete
+    associates in the company.
+-->
 <?php
- include_once 'footer.php';
+    include_once 'header.php';
 ?>
+
+<br />
+<h2>Current Associates</h2>
+<br />
+
+<?php
+    require_once 'legacydbh.inc.php';
+    require_once 'functions.inc.php';
+    require_once 'adminfunctions.inc.php';
+
+    function show_all_assoc()
+    {
+        echo
+        ("
+            <table border=solid>
+            <tr>
+            <th> ID </th>
+            <th> Username </th>
+            <th> First Name </th>
+            <th> Last Name </th>
+            <th> Commission </th>
+            </tr>
+        ");
+        $ID = associateIDS();
+        foreach($ID as $arrID)
+        {
+            $assoc = associateinfo($arrID);
+
+            echo ("<tr>");
+                echo ("<td>" . $arrID . "</td>");
+                echo ("<td>" . $assoc['username'] . "</td>");
+                echo ("<td>" . $assoc['First_name'] . "</td>");
+                echo ("<td>" . $assoc['last_name'] . "</td>");
+                echo ("<td>" . $assoc['commission'] . "</td>");
+                echo '<td><a href="update_assoc.php?id='.$arrID.'">Update</a></t                                                                                            d>';
+                echo '<td><a href="delete_assoc.php?id='.$arrID.'">Delete</a></t                                                                                            d>';
+            echo ("</tr>");
+        }
+    }
+
+    try
+    {
+        show_all_assoc();
+    }
+    catch(PDOexception $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
+
 
