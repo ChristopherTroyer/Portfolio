6706234b17e6ae24463126f0aa23be47c8b236a4
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-05-05 14:11:13 -0500

    Update search_quotes.php

--- search_quotes.php
+++ search_quotes.php
@@ -6,212 +6,48 @@
     require_once 'includes/legacydbh.inc.php'; // legacy database handler
     require_once 'includes/functions.inc.php';
 
-    function showquotes($table)
-    {
-        //setting up table build
-        echo"<table border='1'>";
-        echo "<tr>";
-
-        //shows what each thing is
-        foreach($table[0] as $entity => $type)
-        {
-            echo "<th>$entity</th>";
-        }
-        echo "</tr>";
-
-        foreach($table as $tables)
-        {
-            //qoute ID to identify each item
-            $qid = '';
-
-            //make new rows to the table since the other ones were
-            //set. This will have info
-            echo"<tr>";
-            foreach($table as $entity => $type)
-            {
-                echo"<td>$type</td>"; //shows info
-            }
-
-            echo'<td><a href ="view_quotes.php/?specQuote=' .$table['QuoteID'].'">View Quote</a></td>';
-            echo"</tr>";
-        }
-    }
-
-    try
-    {
-
-        //search by drop down...status
-        if(isset($_POST['status']))
-        {
-            if($_POST['status'] == 'show all')
-            {
-                $select = $pdo->query("SELECT * FROM New_Quote;");
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                $stat = $_POST['status'];
-                $statinfo = "SELECT * FROM New_Quote WHERE status = '$stat';";
-                $temp = $pdo->query($statinfo);
-                $row = $temp->fetchAll(PDO::FETCH_ASSOC);
-
-                echo("<br /><h3>" .$stat. "records.</h3>");
-
-                if($row != false)
-                {
-                    showquotes($row);
-                }
-                else
-                {
-                    echo "<h3>No records.</h3>";
-                }
-            }
-        }
-        else
-        {
-            $select = $pdo->query("SELECT * FROM New_Quote;");
-            if($select != false)
-            {
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                echo "<h3>No records.</h3>";
-            }
-        }
-
-        $selectsearch="SELECT DISTINCT status FROM New_Quote";
-
-        echo "<br><form method='post'><select name=status onchange='javascript: submit()' value=''>Status</option>";
-        echo"<option value=all>status...</option>";
-
-        foreach($pdo->query($selectsearch) as $stattable)
-        {
-            echo"<option value=$stattable[status]>$stattable[status]</option>";
-        }
-        echo"</select></form>";
+    $username = "debian-sys-maint";
+    $password = "vUlvuFil887Af63z";
+    $dbname = "testing";
+    $pdo = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
 
 
+    $output = $pdo->prepare('SELECT * FROM Associate, New_Quote WHERE New_Quotes.AssocID = AssocID');
+    $output->execute();
+    $qid = $output->fetchAll(PDO::FETCH_ASSOC);
 
-        //search by associate ID
-        if(isset($_POST['AssocID']))
-        {
-            if($_POST['AssocID'] == 'show all')
-            {
-                $select = $pdo->query("SELECT * FROM New_Quote;");
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                $assoc = $_POST['AssocID'];
-                $associnfo = "SELECT * FROM New_Quote WHERE AssocID = '$assoc';";
-                $temp = $pdo->query($associnfo);
-                $row = $temp->fetchAll(PDO::FETCH_ASSOC);
+    $output = $pdo->prepare('SELECT * FROM New_Quote, Line_Item WHERE QuoteID.ItemID=Line_Items.QuoteID');
+    $output->execute();
+    $litem = $output ->fetchAll(PDO::FETCG_ASSOC);
 
-                echo("<br /><h3>" .$assoc. "records.</h3>");
-
-                if($row != false)
-                {
-                    showquotes($row);
-                }
-                else
-                {
-                    echo "<h3>No records.</h3>";
-                }
-            }
-        }
-        else
-        {
-            $select = $pdo->query("SELECT * FROM New_Quote;");
-            if($select != false)
-            {
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                echo"<h3>No records.</h3>";
-            }
-        }
-
-        $assocsearch="SELECT DISTINCT AssocID FROM New_Quote;";
-
-        echo "<br><form method='post'><select name=status onchange='javascript: submit()' value=''>AssocID</option>";
-        echo"<option value=all>Associate...</option>";
-
-        foreach($pdo->query($assocsearch) as $assoctable)
-        {
-            echo"<option value=$assoctable[AssocID]>$stattable[AssocID]</option>";
-        }
-        echo"</select></form>";
-
-
-
-        //search by customer id
-        if(isset($_POST['CustID']))
-        {
-            if($_POST['CustID'] == 'show all')
-            {
-                $select = $pdo->query("SELECT * FROM New_Quote;");
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                $cust = $_POST['CustID'];
-                $custinfo = "SELECT * FROM New_Quote WHERE CustID = '$cust';";
-                $temp = $pdo->query($custinfo);
-                $row = $temp->fetchAll(PDO::FETCH_ASSOC);
-
-                echo("<br /><h3>" .$cust. "records.</h3>");
-
-                if($row != false)
-                {
-                    showquotes($row);
-                }
-                else
-                {
-                    echo "<h3>No records.</h3>";
-                }
-            }
-        }
-        else
-        {
-            $select = $pdo->query("SELECT * FROM New_Quote;");
-
-            if($select != false)
-            {
-                $table = $select->fetchAll(PDO::FETCH_ASSOC);
-                showquotes($table);
-            }
-            else
-            {
-                 echo "<h3>No records.</h3>";
-            }
-        }
-
-        $custsearch="SELECT DISTINCT CustID FROM New_Quote;";
-
-        echo "<br><form method='post'><select name=status onchange='javascript: submit()' value=''>CustID</option>";
-        echo"<option value=all>Customer ID...</option>";
-
-        foreach($pdo->query($custsearch) as $stattable)
-        {
-            echo"<option value=$stattable[CustID]>$stattable[CustID]</option>";
-        }
-        echo"</select></form>";
-
-
-    }
-    catch(PDOexception $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-    }
+    $output = $conn->prepare('SELECT * FROM customers');
+    $output->execute();
+    $cust = $output->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<?php
-    include 'footer.php';
-?>
+    <h2>Quotes</h2>
+        <table>
+        <thead>
+        <tr>
+            <td>ID</td>
+            <td>Associate</td>
+            <td>Customer</td>
+            <td>total</td>
+            <td>status</td>
+        </tr>
+        </thead>
+        <tbody>
+            <?php foreach($row as $quote): ?>
+                <tr>
+                    <td><?=$quote['QuoteID']?></td>
+                    <td><?=$quote['First_name']. ' ' .$quote['last_name']?></td>
+                    <td><?=$quote['CustID']?></td>
+                    <td><?=$quote['price']?></td>
+                    <td><?=$quote['process_date']?></td>
+                    <td class="buttom">
+                        <a href="view_quote.php?QuoteID=<?=$quote['QuoteID']?>" class="button"></a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+        </table>
