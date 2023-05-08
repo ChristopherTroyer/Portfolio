11ef1af00051bc53e6488acdd85c6fa09d454157
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-05-08 17:50:33 -0500

    Update delete_assoc.php
    added comments throught so people can see what is happening

--- delete_assoc.php
+++ delete_assoc.php
@@ -9,25 +9,30 @@
     include 'header.php';
 ?>
 
-<h3>WARNING: IF YOU DELETE THE ASSOCIATE WILL BE GONE FOREVER</h3>
+<h3>WARNING: IF YOU DELETE THE EMPLOYEE WILL BE GONE FOREVER</h3>
 
 <?php
-    require_once 'includes/legacydbh.inc.php';
-    require_once 'includes/functions.inc.php';
+    require_once 'includes/legacydbh.inc.php'; //includes legacy dbh just in case I need to use it
+    require_once 'includes/functions.inc.php'; //includes error checking functions and login  checking as well
 
     try
     {
+        //this is used because the legacy db and the dbh are both $conn and I need another name
+        //in case I need legacy
         $username = "debian-sys-maint";
         $password = "vUlvuFil887Af63z";
         $dbname = "testing";
         $pdo = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password); 
-
+        
+        //if its going to be deleted
         if(isset($_POST["DELETED"]))
         {
+            //delete all information from the associate which matches the dropdown
             $arr = $pdo->prepare("DELETE FROM Associate WHERE AssocID = ?");
             $arr->execute(array($_GET['AssocID']));
             $remove = $arr->fetchAll(PDO::FETCH_ASSOC);
         }
+        //this is you do not want to delete the information
         if(isset($_POST["BACK"]))
         {
             header('location:admin.php');
@@ -40,6 +45,7 @@
 
 ?>
 
+<!-- Buttons to double check you want to delete this person -->
 <body>
     <div class="buttons">
         <form method="POST">
