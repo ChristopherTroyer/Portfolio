2ea8fff0ff18fe2ea0a7cd9273738cd91e620b76
Z1917365 <112107209+Z1917365@users.noreply.github.com>
2023-05-08 19:07:07 -0500

    Update update_assoc_action.php
    added comments to the error catching page

--- update_assoc_action.php
+++ update_assoc_action.php
@@ -7,7 +7,6 @@
 <?php
     require_once 'includes/legacydbh.inc.php';
     require_once 'includes/functions.inc.php';
-    require_once 'adminfunctions.inc.php';
 
     if (isset($_POST['update']))
     {
@@ -22,56 +21,57 @@
         $permission = mysqli_real_escape_string($mysqli, $_POST['permission']);
         $email = mysqli_real_escape_string($mysqli, $_POST['email']);
 
-        // Check for empty fields
+        // Check for empty fields and output errors if the fields are emptiy
         if (empty($aid) || empty($first) || empty($last) || empty($username) || empty($password) || empty($addy) || empty($commission) || empty($permission) || empty($email) )
         {
             if (empty($aid))
             {
-                echo "<font color='red'>Name field is empty.</font><br/>";
+                echo "<font color='red'>Associate ID field is empty.</font><br/>"; //associate ID
             }
 
             if (empty($first))
             {
-                echo "<font color='red'>Age field is empty.</font><br/>";
+                echo "<font color='red'>First Name field is empty.</font><br/>"; //first name
             }
 
             if (empty($last))
             {
-                echo "<font color='red'>Email field is empty.</font><br/>";
+                echo "<font color='red'>Last Name field is empty.</font><br/>"; //last name
             }
         
             if (empty($username))
             {
-                echo "<font color='red'>Name field is empty.</font><br/>";
+                echo "<font color='red'>Username field is empty.</font><br/>"; //username
             }
 
             if (empty($password))
             {
-                echo "<font color='red'>Age field is empty.</font><br/>";
+                echo "<font color='red'>Password field is empty.</font><br/>"; //password
             }
 
             if (empty($addy))
             {
-                echo "<font color='red'>Email field is empty.</font><br/>";
+                echo "<font color='red'>Address field is empty.</font><br/>"; //address
             }
         
             if (empty($commission))
             {
-                echo "<font color='red'>Name field is empty.</font><br/>";
+                echo "<font color='red'>Commission field is empty.</font><br/>"; //commission
             }
 
             if (empty($permission))
             {
-                echo "<font color='red'>Age field is empty.</font><br/>";
+                echo "<font color='red'>Permission field is empty.</font><br/>"; //permission
             }
 
             if (empty($email))
             {
-                echo "<font color='red'>Email field is empty.</font><br/>";
+                echo "<font color='red'>Email field is empty.</font><br/>"; //email
             }
         }
         else
         {
+            //if the fields being updated are not empty then update the associates information
             // Update the dbh
             $result = mysqli_query($mysqli, "UPDATE users SET `fname` = '$first', `lname` = '$last', `uname` = '$username', `pword` = '$password', `address` = '$addy', `commission` = '$commission', `permission` = '$permission', `email` = '$email' WHERE `id` = $id");
 
